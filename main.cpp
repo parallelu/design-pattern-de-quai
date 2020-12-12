111adf58afc58ad8bbf5f08490e8f13159904d3c
galaxie <will.yxlee@gmail.com>
2020-12-12 13:22:41 -0500

    Create main.cpp
    check boost version

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <boost/version.hpp>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    cout << BOOST_VERSION << endl;
+    return 0;
+}
